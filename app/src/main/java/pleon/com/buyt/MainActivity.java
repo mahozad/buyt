274d4bf9184e72a5c97cc0d825c3eea067e02f06
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2018-11-22 00:46:32 +0330

    Add bottom app bar and fab

--- app/src/main/java/pleon/com/buyt/MainActivity.java
+++ app/src/main/java/pleon/com/buyt/MainActivity.java
@@ -1,7 +1,10 @@
 package pleon.com.buyt;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomAppBar bottomAppBar = findViewById(R.id.bottomBar);
+        bottomAppBar.replaceMenu(R.menu.menu_bottom_home);
     }
 }
