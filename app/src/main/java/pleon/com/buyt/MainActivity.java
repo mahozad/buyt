11e6f728f9665d3689c545120425122fb0f86ae2
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2018-11-22 11:16:57 +0330

    Set and inflate the bottom app bar with menu items

--- app/src/main/java/pleon/com/buyt/MainActivity.java
+++ app/src/main/java/pleon/com/buyt/MainActivity.java
@@ -1,12 +1,22 @@
 package pleon.com.buyt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static com.google.android.material.snackbar.Snackbar.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TODO: implement the app with Flutter
+    // to have both the top and bottom app bars create the activity with top app bar and add
+    // a fragment that includes the bottom app bar in it in this activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomAppBar bottomAppBar = findViewById(R.id.bottomBar);
-        bottomAppBar.replaceMenu(R.menu.menu_bottom_home);
+        setSupportActionBar(bottomAppBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bottom_home, menu);
+        return true;
     }
 }
