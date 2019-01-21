e92e8d8c74b7a40276a2e7c88f63ad4364efb0d0
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-01-22 00:44:36 +0330

    Add initial implementation of date picking for bought item

--- app/src/main/java/com/pleon/buyt/ui/fragment/AddItemFragment.java
+++ app/src/main/java/com/pleon/buyt/ui/fragment/AddItemFragment.java
@@ -1,5 +1,6 @@
 package com.pleon.buyt.ui.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.ColorDrawable;
@@ -10,16 +11,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendar;
 import com.pleon.buyt.R;
 import com.pleon.buyt.model.Item;
 import com.pleon.buyt.model.Quantity;
 import com.pleon.buyt.model.Quantity.Unit;
+import com.pleon.buyt.ui.dialog.DatePickerFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.core.widget.CompoundButtonCompat;
@@ -28,6 +36,7 @@ import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 import butterknife.OnFocusChange;
 import butterknife.OnTextChanged;
 import butterknife.Unbinder;
@@ -36,7 +45,9 @@ import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 import static butterknife.OnTextChanged.Callback.AFTER_TEXT_CHANGED;
 
-public class AddItemFragment extends Fragment {
+public class AddItemFragment extends Fragment
+        implements DatePickerDialog.OnDateSetListener,
+        com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog.OnDateSetListener {
 
     public interface Callback {
 
@@ -59,6 +70,8 @@ public class AddItemFragment extends Fragment {
     @BindView(R.id.price_layout) TextInputLayout priceTxinlt;
     @BindView(R.id.price) EditText priceEdtx;
     @BindView(R.id.price_container) FrameLayout priceContainer;
+    @BindView(R.id.date_layout) TextInputLayout dateTxinlt;
+    @BindView(R.id.date) EditText dateEdtx;
 
     private Unbinder unbinder;
 
@@ -99,6 +112,69 @@ public class AddItemFragment extends Fragment {
         }
 
         return view;
+    }
+
+    // FIXME: If date picker dialog is shown (at least once) and a configuration change happens
+    // (e.g. screen rotation), then the date picker is shown. This problem seems to have nothing
+    // to do with the following two method calling each other.
+
+    /**
+     * DatePickerDialog.show() for persian calendar is passed getActivity().getFragmentManager()
+     * because it requires android.app.FragmentManager instead of androidx version.
+     * <p>
+     * DatePickerFragment.show() is passed getChildFragmentManager() can get the parent (this)
+     * fragment and set it as the callback.
+     */
+    @OnClick(R.id.date)
+    void onDateClicked() {
+        if (getActivity().getResources().getConfiguration().locale.getDisplayName().equals("فارسی (ایران)")) {
+            PersianCalendar persianCal = new PersianCalendar();
+            com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog datePicker = com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog
+                    .newInstance(this, persianCal.getPersianYear(), persianCal.getPersianMonth(), persianCal.getPersianDay());
+            datePicker.setThemeDark(true); // if you want to change colors see colors.xml
+            datePicker.show(getActivity().getFragmentManager(), "DATE_PICKER");
+        } else {
+            new DatePickerFragment().show(getChildFragmentManager(), "DATE_PICKER");
+        }
+    }
+
+    /**
+     * Calls the onClick method of the date field.
+     * <p>
+     * This method is required because if focusable is set to true (and that is required
+     * because of the textField border to change color), then the first click on the text view
+     * will not open the date picker dialog because if the field does not have focus the first
+     * click on it only requests focus.
+     *
+     * @param focused if the view is focused
+     */
+    @OnFocusChange(R.id.date)
+    void onDateGainedFocus(boolean focused) {
+        if (focused) {
+            onDateClicked();
+        }
+    }
+
+    // For result of Persian date picker
+    @Override
+    public void onDateSet(com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        PersianCalendar cal = new PersianCalendar();
+        cal.set(year, monthOfYear, dayOfMonth);
+
+        String myFormat = "yyyy/MM/dd";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        dateEdtx.setText(dateFormat.format(cal.getTime()));
+    }
+
+    // For result of Gregorian date picker
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month, dayOfMonth);
+
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        dateEdtx.setText(dateFormat.format(cal.getTime()));
     }
 
     @OnCheckedChanged(R.id.urgent)
