9ac9efc6ab38b68efdbaa8c24b2f70bb8be4f68e
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-01-26 18:02:16 +0330

    Move data storing from onPause to onStop method

--- app/src/main/java/com/pleon/buyt/ui/fragment/ItemListFragment.java
+++ app/src/main/java/com/pleon/buyt/ui/fragment/ItemListFragment.java
@@ -117,8 +117,8 @@ public class ItemListFragment extends Fragment implements ItemTouchHelperListene
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onStop() {
+        super.onStop();
         if (itemsReordered) {
             mainViewModel.updateItems(adapter.getItems());
             itemsReordered = false;
