85838ed8257d9d9d26965600813bf5748a0e90a8
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-02-15 22:57:34 +0330

    Update periods

--- app/src/main/java/com/pleon/buyt/viewmodel/StatisticsViewModel.java
+++ app/src/main/java/com/pleon/buyt/viewmodel/StatisticsViewModel.java
@@ -2,10 +2,12 @@ package com.pleon.buyt.viewmodel;
 
 import android.app.Application;
 
+import com.pleon.buyt.R;
 import com.pleon.buyt.database.repository.StatisticsRepository;
 import com.pleon.buyt.model.Category;
 import com.pleon.buyt.model.Statistics;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
@@ -16,12 +18,19 @@ public class StatisticsViewModel extends AndroidViewModel {
 
     public enum Period {
 
-        NARROW(15), MEDIUM(30), EXTENDED(60), WIDE(90);
+        NARROW(7, R.drawable.avd_period_wid_nar), MEDIUM(15, R.drawable.avd_period_nar_med),
+        EXTENDED(30, R.drawable.avd_period_med_ext), WIDE(90, R.drawable.avd_period_ext_wid);
 
         public int length;
+        @DrawableRes private int imageRes;
 
-        Period(int length) {
+        Period(int length, int imageRes) {
             this.length = length;
+            this.imageRes = imageRes;
+        }
+
+        public int getImageRes() {
+            return imageRes;
         }
     }
 
