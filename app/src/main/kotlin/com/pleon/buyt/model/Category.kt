a482ed9c28f41456302d3829e976a39f71012f4a
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-03-23 22:43:39 +0430

    Update and localize category names

--- app/src/main/kotlin/com/pleon/buyt/model/Category.kt
+++ app/src/main/kotlin/com/pleon/buyt/model/Category.kt
@@ -16,7 +16,7 @@ enum class Category(val nameRes: Int, val storeNameRes: Int, val imageRes: Int, 
     FAST_FOOD(R.string.item_cat_fast_food, R.string.store_cat_fast_food, R.drawable.ic_item_fast_food, R.drawable.ic_store_fast_food),
     ICE_CREAM(R.string.item_cat_ice_cream, R.string.store_cat_icecream_shop, R.drawable.ic_item_icecream, R.drawable.ic_store_ice_creamy),
     PASTRY(R.string.item_cat_pastry, R.string.store_cat_confectionery, R.drawable.ic_item_pastry, R.drawable.ic_store_pastry),
-    // VEGETABLE(R.string.item_cat_vegetable, R.drawable.ic_item_vegetable),
+    TOOLS(R.string.item_cat_tools, R.string.store_cat_hardware_store, R.drawable.ic_item_tools, R.drawable.ic_store_hardwary)
     // DRUG_STORE(R.string.store_cat_drugstore, R.drawable.ic_store_ice_creamy),
     // FLOWER_SHOP(R.string.store_cat_flower_shop, R.drawable.ic_store_ice_creamy),
     // DRY_CLEANERS(R.string.store_cat_dry_cleaners, R.drawable.ic_store_ice_creamy),
@@ -24,6 +24,5 @@ enum class Category(val nameRes: Int, val storeNameRes: Int, val imageRes: Int, 
     // BOOKSHOP(R.string.store_cat_book_shop, R.drawable.ic_store_ice_creamy),
     // CLOTHES_SHOP(R.string.store_cat_clothes_shop, R.drawable.ic_store_ice_creamy),
     // SHOE_SHOP(R.string.store_cat_shoe_shop, R.drawable.ic_store_ice_creamy),
-    // OTHER(R.string.item_cat_other, R.drawable.ic_item_other),
-    TOOLS(R.string.item_cat_tools, R.string.store_cat_hardware_store, R.drawable.ic_item_tools, R.drawable.ic_store_hardwary)
+    // OTHER(R.string.item_cat_other, R.drawable.ic_item_other)
 }
