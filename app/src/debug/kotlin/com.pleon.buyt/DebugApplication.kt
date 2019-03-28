4020905aee02deb7c30668c5dfd1e0cf5b93ce22
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-03-28 22:27:31 +0430

    Rename class

--- app/src/debug/kotlin/com.pleon.buyt/DebugApplication.kt
+++ app/src/debug/kotlin/com.pleon.buyt/DebugApplication.kt
@@ -1,17 +1,16 @@
 package com.pleon.buyt
 
 import android.app.Application
-
 import com.facebook.stetho.Stetho
 
-/*
+/**
  * if application runs in debug mode, this file will be the "Application" class of the app
  * (we've defined so in the manifest file of the src/debug).
  * Here the Stetho tool is setup so we can inspect app database, network, layouts and so on
  * with Google chrome in: [chrome://inspect].
  * In Iran, use VPN due to sanctions.
  */
-class BuytApplication : Application() {
+class DebugApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
