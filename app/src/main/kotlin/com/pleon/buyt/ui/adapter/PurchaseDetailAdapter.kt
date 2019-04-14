95701efb488fa12f69ac61f09ae9127a6d20c3bf
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-04-15 00:09:40 +0430

    Add date header to the purchase details screen

--- app/src/main/kotlin/com/pleon/buyt/ui/adapter/PurchaseDetailAdapter.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/adapter/PurchaseDetailAdapter.kt
@@ -1,18 +1,25 @@
 package com.pleon.buyt.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.pleon.buyt.R
 import com.pleon.buyt.model.PurchaseDetail
 import com.pleon.buyt.ui.BaseViewHolder
-import com.pleon.buyt.ui.adapter.PurchaseDetailAdapter.PurchaseHolder
+import com.pleon.buyt.ui.DateHeaderDecoration.StickyHeaderInterface
+import ir.huri.jcal.JalaliCalendar
+import kotlinx.android.synthetic.main.date_header.view.*
 import kotlinx.android.synthetic.main.purchase_detail.view.*
+import org.jetbrains.anko.configuration
+import java.util.*
 
-class PurchaseDetailAdapter : RecyclerView.Adapter<PurchaseHolder>() {
+class PurchaseDetailAdapter(private val cxt: Context) : Adapter<ViewHolder>(), StickyHeaderInterface {
 
-    var items = mutableListOf<PurchaseDetail>()
+    var items = mutableListOf<Any>()
         set(value) {
             field = value
             notifyDataSetChanged()
@@ -20,14 +27,53 @@ class PurchaseDetailAdapter : RecyclerView.Adapter<PurchaseHolder>() {
 
     override fun getItemCount() = items.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PurchaseHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.layout.purchase_detail, parent, false)
-        return PurchaseHolder(itemView)
+    override fun getItemViewType(position: Int): Int {
+        // Just as an example, return 0 or 1 depending on position
+        // Note that unlike in ListView adapters, types don't have to be contiguous
+        return if (items[position] is PurchaseDetail) 0 else 1
     }
 
-    override fun onBindViewHolder(holder: PurchaseHolder, position: Int) {
-        holder.bindItem(items[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            0 -> PurchaseHolder(inflater.inflate(com.pleon.buyt.R.layout.purchase_detail, parent, false))
+            else -> DateHolder(inflater.inflate(com.pleon.buyt.R.layout.date_header, parent, false))
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            0 -> (holder as PurchaseHolder).bindItem(items[position] as PurchaseDetail)
+            else -> (holder as DateHolder).bindItem(items[position] as Date)
+        }
+    }
+
+    override fun getHeaderPositionForItem(itemPosition: Int): Int {
+        var itemPosition = itemPosition
+        var headerPosition = 0
+        do {
+            if (this.isHeader(itemPosition)) {
+                headerPosition = itemPosition
+                break
+            }
+            itemPosition -= 1
+        } while (itemPosition >= 0)
+        return headerPosition
+    }
+
+    override fun bindHeaderData(header: View, headerPosition: Int) {
+        header.findViewById<TextView>(R.id.dateText).text = formatDate(items[headerPosition] as Date)
+    }
+
+    override fun getHeaderLayout(headerPosition: Int) = com.pleon.buyt.R.layout.date_header
+
+    override fun isHeader(itemPosition: Int) = items[itemPosition] is Date
+
+    private fun formatDate(date: Date): String {
+        val jalaliCalendar = JalaliCalendar(date)
+        return String.format(cxt.configuration.locale, "%s %d %s %d",
+                jalaliCalendar.dayOfWeekString, jalaliCalendar.day,
+                jalaliCalendar.monthString, jalaliCalendar.year)
     }
 
     inner class PurchaseHolder(view: View) : BaseViewHolder(view) {
@@ -37,6 +83,13 @@ class PurchaseDetailAdapter : RecyclerView.Adapter<PurchaseHolder>() {
             var totalCost = 0L
             for (item in purchaseDetail.item) totalCost += item.totalPrice
             itemView.totalCost.text = totalCost.toString()
+        }
+
+    }
+
+    inner class DateHolder(view: View) : BaseViewHolder(view) {
+        fun bindItem(date: Date) {
+            itemView.dateText.text = formatDate(date)
         }
     }
 }
--- app/src/main/kotlin/com/pleon/buyt/ui/fragment/StateDetailsFragment.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/fragment/StateDetailsFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import com.pleon.buyt.R
 import com.pleon.buyt.model.Statistics
+import com.pleon.buyt.ui.DateHeaderDecoration
 import com.pleon.buyt.ui.adapter.PurchaseDetailAdapter
 import com.pleon.buyt.viewmodel.StatisticsViewModel
 import kotlinx.android.synthetic.main.fragment_state_details.*
+import java.text.SimpleDateFormat
+
 
 class StateDetailsFragment : Fragment() {
 
@@ -18,14 +20,27 @@ class StateDetailsFragment : Fragment() {
     private lateinit var viewModel: StatisticsViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedState: Bundle?)
-            : View = inflater.inflate(R.layout.fragment_state_details, container, false)
+            : View = inflater.inflate(com.pleon.buyt.R.layout.fragment_state_details, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel = ViewModelProviders.of(activity!!).get(StatisticsViewModel::class.java)
-        adapter = PurchaseDetailAdapter().also { recyclerView.adapter = it }
+        adapter = PurchaseDetailAdapter(context!!)
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(DateHeaderDecoration(recyclerView, adapter))
     }
 
     fun showStats(stats: Statistics) {
-        adapter.items = stats.purchaseDetails!!.toMutableList()
+        val list = mutableListOf<Any>()
+        val fmt = SimpleDateFormat("yyyyMMdd")
+        var date = stats.purchaseDetails!![0].purchase.date
+        list.add(date)
+        for (detail in stats.purchaseDetails!!) {
+            if (fmt.format(detail.purchase.date) != fmt.format(date)) {
+                list.add(detail.purchase.date)
+                date = detail.purchase.date
+            }
+            list.add(detail)
+        }
+        adapter.items = list
     }
 }
