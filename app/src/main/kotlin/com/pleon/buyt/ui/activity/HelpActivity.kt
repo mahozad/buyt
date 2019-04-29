f01a688804b55fbb1c93b00c405fb5413bcebc77
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-04-29 22:14:31 +0430

    Update help screen

--- app/src/main/kotlin/com/pleon/buyt/ui/activity/HelpActivity.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/activity/HelpActivity.kt
@@ -1,53 +1,51 @@
 package com.pleon.buyt.ui.activity
 
+import android.content.Intent
 import android.graphics.drawable.Animatable
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.view.Menu
 import android.view.MenuItem
 import android.view.animation.AlphaAnimation
-import com.pleon.buyt.R
 import kotlinx.android.synthetic.main.activity_help.*
 
 class HelpActivity : BaseActivity() {
 
-    override fun layout() = R.layout.activity_help
+    override fun layout() = com.pleon.buyt.R.layout.activity_help
 
     override fun onCreate(savedState: Bundle?) {
         super.onCreate(savedState)
+        waveHeader.start()
+        animateBrand()
         logo.setOnClickListener {
-            logo.setImageResource(R.drawable.avd_logo)
+            logo.setImageResource(com.pleon.buyt.R.drawable.avd_logo)
             (logo.drawable as Animatable).start()
         }
-        waveHeader.start()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_bottom_help, menu)
+        menuInflater.inflate(com.pleon.buyt.R.menu.menu_bottom_help, menu)
         return true
     }
 
-    override fun onResume() {
-        super.onResume()
+    private fun animateBrand() {
         Handler().postDelayed({ (logo.drawable as Animatable).start() }, 300)
         Handler().postDelayed({
-            textView10.startAnimation(AlphaAnimation(0F, 1F).apply {
+            nameVersion.startAnimation(AlphaAnimation(0F, 1F).apply {
                 duration = 300
-                textView10.alpha = 1F
+                nameVersion.alpha = 1F
             })
-        }, 400)
-        Handler().postDelayed({
-            textView11.startAnimation(AlphaAnimation(0F, 1F).apply {
-                duration = 300
-                textView11.alpha = 1F
-            })
-        }, 800)
+        }, 500)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> finish()
-            // other menu options
+            com.pleon.buyt.R.id.action_translate -> {
+                val uri = Uri.parse("https://pleonco.oneskyapp.com/collaboration/project?id=158739")
+                startActivity(Intent(Intent.ACTION_VIEW, uri))
+            }
         }
         return true
     }
