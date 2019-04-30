18bc22b6041f42cd7836bc1da2cf4f3ae1e64b6e
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-04-30 18:23:04 +0430

    Improve code

--- app/src/main/kotlin/com/pleon/buyt/ui/SnackbarUtil.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/SnackbarUtil.kt
@@ -1,20 +1,18 @@
 package com.pleon.buyt.ui
 
 import android.view.View
-import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.BaseTransientBottomBar.BaseCallback
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.snackbar.Snackbar.LENGTH_LONG
-import com.google.android.material.snackbar.Snackbar.make
 import com.pleon.buyt.R
 
-fun showUndoSnackbar(snbContainer: View, message: String,
-                     onUndo: () -> Unit, onDismiss: () -> Unit) {
-    val snackbar = make(snbContainer, message, LENGTH_LONG)
+fun showUndoSnackbar(container: View, message: String, onUndo: () -> Unit, onDismiss: () -> Unit) {
+    val snackbar = Snackbar.make(container, message, LENGTH_LONG)
 
     snackbar.setAction(R.string.snackbar_action_undo) { onUndo() }
-    snackbar.addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar>() {
-        override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
-            // If dismiss wasn't because of "UNDO" then delete the store from database
+    snackbar.addCallback(object : BaseCallback<Snackbar>() {
+        override fun onDismissed(bar: Snackbar?, event: Int) {
+            // If dismiss wasn't because of "UNDO" then call listener
             if (event != DISMISS_EVENT_ACTION) onDismiss()
         }
     })
