57d672845515b29d7b4e028f69a80630219532e4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-05-05 00:58:31 +0430

    Remove redundant fragment class

--- app/src/main/kotlin/com/pleon/buyt/ui/adapter/IntroPagerAdapter.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/adapter/IntroPagerAdapter.kt
@@ -1,17 +1,18 @@
 package com.pleon.buyt.ui.adapter
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.pleon.buyt.ui.fragment.intro.IntroFragment1
+import com.pleon.buyt.R
 
 class IntroPagerAdapter(fragMgr: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragMgr, lifecycle) {
 
     override fun getItemCount() = 3
 
     override fun getItem(position: Int) = when (position) {
-        0 -> IntroFragment1()
-        1 -> IntroFragment1()
-        else -> IntroFragment1()
+        0 -> Fragment(R.layout.fragment_intro_1)
+        1 -> Fragment(R.layout.fragment_intro_2)
+        else -> Fragment(R.layout.fragment_intro_2)
     }
 }
--- app/src/main/kotlin/com/pleon/buyt/ui/fragment/intro/IntroFragment1.kt
+++
@@ -1,8 +0,0 @@
-package com.pleon.buyt.ui.fragment.intro
-
-import androidx.fragment.app.Fragment
-import com.pleon.buyt.R
-
-class IntroFragment1 : Fragment(R.layout.fragment_intro_1) {
-
-}
