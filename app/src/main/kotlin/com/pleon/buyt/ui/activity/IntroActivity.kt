fa6db1a4e0ac57dc62d4279b80b4e5726e9d39ce
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-05-05 00:26:22 +0430

    Complete page indicator

--- app/src/main/kotlin/com/pleon/buyt/ui/activity/IntroActivity.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/activity/IntroActivity.kt
@@ -3,23 +3,28 @@ package com.pleon.buyt.ui.activity
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.util.TypedValue
+import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
-import com.davidmedenjak.pagerindicator.ViewPager2PagerListener
 import com.google.android.material.animation.ArgbEvaluatorCompat
 import com.pleon.buyt.R
 import com.pleon.buyt.ui.adapter.IntroPagerAdapter
 import kotlinx.android.synthetic.main.activity_intro.*
+import kotlin.math.max
 import kotlin.math.min
 
 class IntroActivity : BaseActivity() {
 
     var pagerLastPage = 0
+    var pagerLastOffset = 0f
+    lateinit var dots: Array<ImageView>
 
     override fun layout() = R.layout.activity_intro
 
     override fun onCreate(savedState: Bundle?) {
         super.onCreate(savedState)
+
+        dots = arrayOf(dot1, dot2, dot3)
 
         val colors = intArrayOf(
                 resources.getColor(R.color.bottomBarDarkBgColor),
@@ -29,7 +34,6 @@ class IntroActivity : BaseActivity() {
 
         val adapter = IntroPagerAdapter(supportFragmentManager, lifecycle)
         viewPager.adapter = adapter
-        indicator.setPager(ViewPager2PagerListener(viewPager))
 
         viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
             private val argbEvaluator = ArgbEvaluatorCompat()
@@ -47,6 +51,14 @@ class IntroActivity : BaseActivity() {
                 } else {
                     viewPager.setBackgroundColor(colors[colors.size - 1])
                 }
+
+                if (positionOffset >= pagerLastOffset) {
+                    dots[position].alpha = max(1 - positionOffset, 0.2f)
+                    if (position < adapter.itemCount - 1) dots[position + 1].alpha = max(0.2f, positionOffset)
+                } else {
+                    dots[position].alpha = max(positionOffset, 0.2f)
+                    if (position > 0) dots[position - 1].alpha = max(1 - positionOffset, 0.2f)
+                }
             }
 
             override fun onPageSelected(position: Int) {
@@ -61,25 +73,11 @@ class IntroActivity : BaseActivity() {
             }
         })
 
-
         backButton.setOnClickListener { viewPager.currentItem = viewPager.currentItem - 1 }
         nextButtonIcon.setOnClickListener {
             viewPager.currentItem = min(viewPager.currentItem + 1, adapter.itemCount - 1)
         }
-
-
-        /* addSlide(SimpleSlide.Builder()
-                .title(R.string.dialog_title_select_lang)
-                .description("dyfghjlgkk;jbhkl")
-                .image(R.drawable.ic_sigma)
-                .background(R.color.colorPrimary)
-                .backgroundDark(R.color.colorPrimaryDark)
-                .scrollable(false)
-                // .permission(Manifest.permission.CAMERA)
-                .buttonCtaLabel("111") // label for permission button
-                .build())
-        */
     }
 
-//    override fun onBackPressed() {}
+    // override fun onBackPressed() {}
 }
