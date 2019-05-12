d5e91cfbf2eb8a28f7abeccdce8d00d31d9dfbe8
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-05-13 00:50:53 +0430

    Refactor code

--- app/src/main/kotlin/com/pleon/buyt/ui/activity/IntroActivity.kt
+++ app/src/main/kotlin/com/pleon/buyt/ui/activity/IntroActivity.kt
@@ -3,11 +3,13 @@ package com.pleon.buyt.ui.activity
 import android.graphics.Color
 import android.graphics.drawable.Animatable
 import android.os.Bundle
-import android.util.TypedValue
+import android.util.TypedValue.COMPLEX_UNIT_DIP
+import android.util.TypedValue.applyDimension
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.graphics.ColorUtils
 import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.google.android.material.animation.ArgbEvaluatorCompat
 import com.pleon.buyt.R
@@ -20,63 +22,39 @@ import kotlin.math.min
 
 class IntroActivity : BaseActivity() {
 
-    var lastPage = 0
-    var lastOffset = 0f
-    lateinit var dots: Array<ImageView>
+    private lateinit var adapter: FragmentStateAdapter
+    private lateinit var colors: IntArray
+    private lateinit var dots: Array<ImageView>
+    private val argbEvaluator = ArgbEvaluatorCompat()
+    private var lastPage = 0
+    private var lastOffset = 0f
 
     override fun layout() = R.layout.activity_intro
 
     override fun onCreate(savedState: Bundle?) {
         super.onCreate(savedState)
 
+        adapter = IntroPageAdapter(this).also { viewPager.adapter = it }
+        colors = resources.getIntArray(R.array.introPageColors)
         dots = arrayOf(dot1, dot2, dot3)
-        val colors = resources.getIntArray(R.array.introPageColors)
 
-        val adapter = IntroPageAdapter(this)
-        viewPager.adapter = adapter
         viewPager.offscreenPageLimit = 2 // to cache pages for smooth scrolling
+        setupParallaxEffect()
+        for ((i, dot) in dots.withIndex()) dot.setOnClickListener { viewPager.currentItem = i }
 
         viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
-            private val argbEvaluator = ArgbEvaluatorCompat()
 
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-                val params = backButton.layoutParams as ConstraintLayout.LayoutParams
-                val finalPos = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 84F, resources.displayMetrics).toInt()
-                val margin = if (position < 1) (positionOffset * finalPos).toInt() else finalPos
-                params.setMargins(params.leftMargin, params.topMargin, params.rightMargin, margin)
-                backButton.layoutParams = params
-
-                val color = argbEvaluator.evaluate(positionOffset, colors[position], colors[min(position + 1, adapter.itemCount - 1)])
-                parentLayout.setBackgroundColor(color)
-                window.statusBarColor = ColorUtils.blendARGB(color, Color.BLACK, 0.3f)
-
-                if (positionOffset >= lastOffset) {
-                    dots[position].alpha = max(1 - positionOffset, 0.2f)
-                    if (position < adapter.itemCount - 1) dots[position + 1].alpha = max(0.2f, positionOffset)
-                } else {
-                    dots[position].alpha = max(positionOffset, 0.2f)
-                    if (position > 0) dots[position - 1].alpha = max(1 - positionOffset, 0.2f)
-                }
+            override fun onPageScrolled(position: Int, offset: Float, offsetPx: Int) {
+                updatePageAndStatusBarColor(position, offset)
+                updateBackButtonPlacement(position, offset)
+                updateDots(position, offset)
             }
 
             override fun onPageSelected(position: Int) {
-                if (position == adapter.itemCount - 1) {
-                    nextButton.setImageResource(R.drawable.avd_next_done)
-                    (nextButton.drawable as Animatable).start()
-                } else if (position < adapter.itemCount - 1 && lastPage == adapter.itemCount - 1) {
-                    nextButton.setImageResource(R.drawable.avd_done_next)
-                    (nextButton.drawable as Animatable).start()
-                }
-                lastPage = position
+                animateNextButtonIconIfNeeded(position)
+                lastPage = position // should be the last statement
             }
         })
-
-        // For parallax effect
-        viewPager.setPageTransformer { page, pos ->
-            if (pos >= -1 && pos <= 1) page.img.translationX = -pos * page.width / 2
-            else page.alpha = 1f
-        }
 
         backButton.setOnClickListener { viewPager.currentItem = viewPager.currentItem - 1 }
         nextButton.setOnClickListener {
@@ -86,12 +64,50 @@ class IntroActivity : BaseActivity() {
             }
             viewPager.currentItem = min(viewPager.currentItem + 1, adapter.itemCount - 1)
         }
+    }
 
-        for ((index, dot) in dots.withIndex()) {
-            dot.setOnClickListener { viewPager.currentItem = index }
+    override fun onBackPressed() {}
+
+    private fun setupParallaxEffect() {
+        viewPager.setPageTransformer { page, pos ->
+            if (pos >= -1 && pos <= 1) page.img.translationX = -pos * page.width / 2
+            else page.alpha = 1.0f
         }
     }
 
-    override fun onBackPressed() {/* Do nothing */
+    private fun updatePageAndStatusBarColor(position: Int, offset: Float) {
+        val startColor = colors[position]
+        val endColor = colors[min(position + 1, adapter.itemCount - 1)]
+        val color = argbEvaluator.evaluate(offset, startColor, endColor)
+        parentLayout.setBackgroundColor(color)
+        window.statusBarColor = ColorUtils.blendARGB(color, Color.BLACK, 0.3f)
+    }
+
+    private fun updateBackButtonPlacement(position: Int, offset: Float) {
+        val params = backButton.layoutParams as ConstraintLayout.LayoutParams
+        val finalPos = applyDimension(COMPLEX_UNIT_DIP, 84F, resources.displayMetrics).toInt()
+        val bottomMargin = if (position < 1) (offset * finalPos).toInt() else finalPos
+        params.setMargins(params.leftMargin, params.topMargin, params.rightMargin, bottomMargin)
+        backButton.layoutParams = params
+    }
+
+    private fun updateDots(position: Int, offset: Float) {
+        if (offset >= lastOffset) {
+            dots[position].alpha = max(1 - offset, 0.2f)
+            if (position < adapter.itemCount - 1) dots[position + 1].alpha = max(0.2f, offset)
+        } else {
+            dots[position].alpha = max(offset, 0.2f)
+            if (position > 0) dots[position - 1].alpha = max(1 - offset, 0.2f)
+        }
+    }
+
+    private fun animateNextButtonIconIfNeeded(position: Int) {
+        if (position == adapter.itemCount - 1) {
+            nextButton.setImageResource(R.drawable.avd_next_done)
+            (nextButton.drawable as Animatable).start()
+        } else if (position < adapter.itemCount - 1 && lastPage == adapter.itemCount - 1) {
+            nextButton.setImageResource(R.drawable.avd_done_next)
+            (nextButton.drawable as Animatable).start()
+        }
     }
 }
