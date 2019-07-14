421b35a04bcf5fde834f94424a056054170e0aa5
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-07-14 19:54:12 +0430

    Improve code

--- app/src/main/kotlin/com/pleon/buyt/util/VibrationUtil.kt
+++ app/src/main/kotlin/com/pleon/buyt/util/VibrationUtil.kt
@@ -3,7 +3,7 @@ package com.pleon.buyt.util
 import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.O
-import android.os.VibrationEffect
+import android.os.VibrationEffect.createOneShot
 import android.os.Vibrator
 import androidx.core.content.ContextCompat.getSystemService
 
@@ -11,7 +11,7 @@ object VibrationUtil {
 
     fun vibrate(cxt: Context, duration: Long, intensity: Int) {
         val vibrator = getSystemService(cxt, Vibrator::class.java) as Vibrator
-        if (SDK_INT >= O) vibrator.vibrate(VibrationEffect.createOneShot(duration, intensity))
-        else vibrator.vibrate(200)
+        if (SDK_INT >= O) vibrator.vibrate(createOneShot(duration, intensity))
+        else vibrator.vibrate(duration)
     }
 }
