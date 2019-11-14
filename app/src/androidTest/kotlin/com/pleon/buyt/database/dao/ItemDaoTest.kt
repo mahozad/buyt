06a8b0cb49b56bc59431e2ce40f9f7c7b9194788
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-11-14 15:57:11 +0330

    Update and add unit tests

--- app/src/androidTest/kotlin/com/pleon/buyt/database/dao/ItemDaoTest.kt
+++ app/src/androidTest/kotlin/com/pleon/buyt/database/dao/ItemDaoTest.kt
@@ -1,16 +1,20 @@
 package com.pleon.buyt.database.dao
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.platform.app.InstrumentationRegistry
 import com.pleon.buyt.database.AppDatabase
 import com.pleon.buyt.database.InstantExecutorExtension
+import com.pleon.buyt.database.dto.ItemNameCat
+import com.pleon.buyt.model.Category.FRUIT
 import com.pleon.buyt.model.Category.GROCERY
 import com.pleon.buyt.model.Item
 import com.pleon.buyt.model.Item.Quantity
+import com.pleon.buyt.model.Item.Quantity.Unit.GRAM
 import com.pleon.buyt.model.Item.Quantity.Unit.UNIT
-import org.junit.Assert
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -20,6 +24,8 @@ import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
 import java.util.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 /**
  * Instrumentation tests execute only on API level 26 (Android 8.0) and above.
@@ -36,6 +42,24 @@ class ItemDaoTests {
     @Mock
     private lateinit var observer: Observer<List<Item>>
 
+    /**
+     * For unit tests we want the behavior of LiveData to be synchronous,
+     * so we must block the test thread and wait for the value to be passed to the observer.
+     * See [https://stackoverflow.com/a/44991770/8583692]
+     */
+    private fun <T : Any> LiveData<T>.blockingObserve(): T {
+        lateinit var value: T
+        val latch = CountDownLatch(1)
+
+        observeForever {
+            value = it
+            latch.countDown()
+        }
+
+        latch.await(2, TimeUnit.SECONDS)
+        return value
+    }
+
     @BeforeEach
     internal fun setUp() {
         cxt = InstrumentationRegistry.getInstrumentation().context
@@ -51,24 +75,68 @@ class ItemDaoTests {
     }
 
     @Test
-    fun addOneItem() {
-        val item = Item("Chocolate", Quantity(1, UNIT), GROCERY, false, false)
-
+    fun getAll_addOneItem() {
+        val item = Item("Chocolate", Quantity(1, UNIT), GROCERY, isUrgent = false, isBought = false)
         itemDao.insert(item)
 
-        val itemCount = itemDao.getCount()
-        Assert.assertEquals(1, itemCount)
+        val list = itemDao.getAll().blockingObserve()
+
+        assertThat(list.size).isEqualTo(1)
     }
 
     @Test
-    fun deleteOneItem() {
-        val item = Item("Chocolate", Quantity(1, UNIT), GROCERY, false, false)
+    fun getAll_addTwoItems() {
+        val item1 = Item("Chocolate", Quantity(1, UNIT), GROCERY, isUrgent = false, isBought = false)
+        val item2 = Item("Apple", Quantity(500, GRAM), FRUIT, isUrgent = true, isBought = false)
+
+        itemDao.insert(item1)
+        itemDao.insert(item2)
+        val list = itemDao.getAll().blockingObserve()
+
+        assertThat(list.size).isEqualTo(2)
+    }
+
+    @Test
+    fun getCount_addOneItem() {
+        val item = Item("Chocolate", Quantity(1, UNIT), GROCERY, isUrgent = false, isBought = false)
+
+        itemDao.insert(item)
+        val itemCount = itemDao.getCount()
+
+        assertThat(itemCount).isEqualTo(1)
+    }
+
+    @Test
+    fun getCount_addTwoItems() {
+        val item1 = Item("Chocolate", Quantity(1, UNIT), GROCERY, isUrgent = false, isBought = false)
+        val item2 = Item("Apple", Quantity(500, GRAM), FRUIT, isUrgent = true, isBought = false)
+
+        itemDao.insert(item1)
+        itemDao.insert(item2)
+        val itemCount = itemDao.getCount()
+
+        assertThat(itemCount).isEqualTo(2)
+    }
+
+    @Test
+    fun getItemNameCats_addOneNewNameCat() {
+        val item2 = Item("Something", Quantity(500, GRAM), FRUIT, isUrgent = true, isBought = false)
+        itemDao.insert(item2)
+
+        val itemNameCats = itemDao.getItemNameCats().blockingObserve()
+
+        assertThat(itemNameCats[0]).isEqualTo(ItemNameCat("Something", FRUIT))
+    }
+
+    @Test
+    fun delete_oneItem() {
+        val item = Item("Chocolate", Quantity(1, UNIT), GROCERY, isUrgent = false, isBought = false)
 
         itemDao.insert(item).also { item.itemId = it }
         itemDao.delete(item)
 
-        val itemCount = itemDao.getCount()
-        Assert.assertEquals(0, itemCount)
+        assertThat(itemDao.getCount()).isEqualTo(0)
+        assertThat(itemDao.getAll().blockingObserve().size).isEqualTo(0)
     }
 
     @AfterEach
