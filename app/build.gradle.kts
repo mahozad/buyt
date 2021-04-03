12fd93809e546219c6f87ac00ece18f5ebf92411
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-04-03 12:05:13 +0430

    Update app version and its changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.pleon.buyt"
         minSdkVersion(21)
         targetSdkVersion(28)
-        versionCode = 8 // don't forget this
-        versionName = "1.3.2" // alpha -> beta -> rc -> final
+        versionCode = 9 // don't forget this
+        versionName = "1.4.0" // alpha -> beta -> rc -> final
 
         /* This flag prevents the Android Gradle Plugin from generating PNG versions of
          * vector assets if minSdkVersion is < 21 */
