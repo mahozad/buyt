2c0635bcc3c6ce67acc9933042b31b2e6683f5d7
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-04-20 11:53:31 +0430

    Update gradle version from 6.8 to 7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ tasks.wrapper {
     // Add a gradle wrapper script to your source folders (by running the wrapper task).
     // The wrapper script when invoked, downloads the defined gradle version, and executes it.
     // By distributing the wrapper with your project, anyone can work with it without needing to install Gradle beforehand
-    gradleVersion = "6.8"
+    gradleVersion = "7.0"
     // Download the full version of the Gradle (with sources and documentation)
     distributionType = Wrapper.DistributionType.ALL
 }
