46af5d44da49ab4cccb10c3714ca0a0001d2a18b
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-04-22 12:59:38 +0430

    Reformat code

--- app/src/main/kotlin/com/pleon/buyt/database/AppDatabase.kt
+++ app/src/main/kotlin/com/pleon/buyt/database/AppDatabase.kt
@@ -18,8 +18,19 @@ import com.pleon.buyt.model.Subscription
 const val DB_NAME = "buyt-database.db"
 const val DB_VERSION = 1
 
-@Database(entities = [Item::class, Store::class, Purchase::class, Subscription::class], version = DB_VERSION)
-@TypeConverters(DateConverter::class, QuantityUnitConverter::class, CategoryConverter::class)
+@Database(
+        entities = [
+            Item::class,
+            Store::class,
+            Purchase::class,
+            Subscription::class],
+        version = DB_VERSION
+)
+@TypeConverters(
+        DateConverter::class,
+        CategoryConverter::class,
+        QuantityUnitConverter::class
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun itemDao(): ItemDao
