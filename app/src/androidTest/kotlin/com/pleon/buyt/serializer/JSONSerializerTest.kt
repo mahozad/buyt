d5ed1dd5909b8e57232a12bbf0a854dc9729f231
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-06-22 13:44:29 +0430

    Refactor JSONSerializer

---
+++ app/src/androidTest/kotlin/com/pleon/buyt/serializer/JSONSerializerTest.kt
@@ -0,0 +1,92 @@
+package com.pleon.buyt.serializer
+
+import com.pleon.buyt.database.dto.PurchaseDetail
+import com.pleon.buyt.model.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class JSONSerializerTest {
+
+    // FIXME: Inject the dispatcher to the serializer instead of using delay
+    @Test fun shouldProduceCorrectResult() = runBlocking {
+        val serializer = JSONSerializer()
+        val date = Date()
+        val purchaseDetails = listOf(
+            PurchaseDetail().apply {
+                purchase = Purchase(date)
+                item = listOf(
+                    Item("Apple", Item.Quantity(2, Item.Quantity.Unit.KILOGRAM), Category.FRUIT, isUrgent = false, isBought = true),
+                    Item("Ketchup", Item.Quantity(1, Item.Quantity.Unit.UNIT), Category.GROCERY, isUrgent = false, isBought = true)
+                )
+                store = Store(Coordinates(2.3, 5.6), "Store A", Category.GROCERY)
+            },
+            PurchaseDetail().apply {
+                purchase = Purchase(date)
+                item = listOf(
+                    Item("Apple", Item.Quantity(1500, Item.Quantity.Unit.GRAM), Category.FRUIT, isUrgent = true, isBought = true),
+                )
+                store = Store(Coordinates(90.0, 100.0), "Store B", Category.FRUIT)
+            }
+        )
+        val expectedResult = """{ "purchaseDetails": [
+  {
+    "date":${date.time / 1000},
+    "store": {
+      "name":"Store A",
+      "category":"Grocery",
+      "location": {
+        "latitude":2.3,
+        "longitude":5.6
+      }
+    },
+    "items": [
+      {
+        "name":"Apple",
+        "quantity":"2 kg",
+        "description":"-",
+        "totalCost":"0 dollars",
+        "urgency":"-"
+      },
+      {
+        "name":"Ketchup",
+        "quantity":"1 unit",
+        "description":"-",
+        "totalCost":"0 dollars",
+        "urgency":"-"
+      }
+    ]
+  },
+  {
+    "date":${date.time / 1000},
+    "store": {
+      "name":"Store B",
+      "category":"Fruit",
+      "location": {
+        "latitude":90.0,
+        "longitude":100.0
+      }
+    },
+    "items": [
+      {
+        "name":"Apple",
+        "quantity":"1500 g",
+        "description":"-",
+        "totalCost":"0 dollars",
+        "urgency":"!"
+      }
+    ]
+  }
+]}"""
+        var result = ""
+        serializer.updateListener = { _, fragment -> result += fragment }
+        serializer.finishListener = {
+            assertThat(result).isEqualTo(expectedResult)
+        }
+
+        serializer.serialize(purchaseDetails)
+        delay(1000)
+    }
+}
--- app/src/main/kotlin/com/pleon/buyt/serializer/JSONSerializer.kt
+++ app/src/main/kotlin/com/pleon/buyt/serializer/JSONSerializer.kt
@@ -8,7 +8,7 @@ import com.pleon.buyt.database.dto.PurchaseDetail
 import com.pleon.buyt.util.formatPrice
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.koin.java.KoinJavaComponent
+import org.koin.java.KoinJavaComponent.inject
 
 class JSONSerializer: InteractiveSerializer<PurchaseDetail> {
 
@@ -16,51 +16,49 @@ class JSONSerializer: InteractiveSerializer<PurchaseDetail> {
     override val fileExtension = "json"
     override var updateListener: (suspend (Int, String) -> Unit)? = null
     override var finishListener: (suspend () -> Unit)? = null
-    private val application by KoinJavaComponent.inject(Application::class.java)
+    private val application by inject(Application::class.java)
     private val head = "{ \"purchaseDetails\": [\n"
     private val tail = "]}"
 
     override suspend fun serialize(entities: List<PurchaseDetail>): Unit = withContext(Dispatchers.Default) {
-        val stringBuilder = StringBuilder()
         updateListener?.invoke(0, head)
         for ((i, purchaseDetail) in entities.withIndex()) {
-            stringBuilder.append(purchaseElement(purchaseDetail))
-            stringBuilder.append(if (i < entities.size - 1) ",\n" else "\n")
+            val isLast = (i == entities.size - 1)
+            val fragment = purchaseElement(purchaseDetail, isLast)
             val progress = ((i + 1f) / entities.size * 100).toInt()
-            updateListener?.invoke(progress, stringBuilder.toString())
-            stringBuilder.clear()
+            updateListener?.invoke(progress, fragment)
         }
         updateListener?.invoke(100, tail)
         finishListener?.invoke()
     }
 
-    private fun purchaseElement(purchaseDetail: PurchaseDetail): String {
+    private fun purchaseElement(purchaseDetail: PurchaseDetail, isLast: Boolean): String {
         val noValue = getString(R.string.no_value)
-        val stringBuilder = StringBuilder()
         val storeCategory = purchaseDetail.store?.category?.nameRes?.let { getString(it) } ?: noValue
-        stringBuilder.append("  { ").appendLine()
-        stringBuilder.append("    \"date\":${purchaseDetail.purchase.date.time / 1000},").appendLine()
-        stringBuilder.append("    \"store\": { ").appendLine()
-        stringBuilder.append("      \"name\":\"${purchaseDetail.store?.name ?: noValue}\",").appendLine()
-        stringBuilder.append("      \"category\":\"${storeCategory}\",").appendLine()
-        stringBuilder.append("      \"location\": { ").appendLine()
-        stringBuilder.append("        \"latitude\":${purchaseDetail.store?.location?.latitude ?: noValue},").appendLine()
-        stringBuilder.append("        \"longitude\":${purchaseDetail.store?.location?.longitude ?: noValue}").appendLine()
-        stringBuilder.append("      }").appendLine()
-        stringBuilder.append("    },").appendLine()
-        stringBuilder.append("    \"items\": [ ").appendLine()
-        for ((i, item) in purchaseDetail.item.withIndex()) {
-            stringBuilder.append("      { ").appendLine()
-            stringBuilder.append("        \"name\":\"${item.name}\",").appendLine()
-            stringBuilder.append("        \"quantity\":\"${getString(R.string.item_quantity, item.quantity.value, getString(item.quantity.unit.nameRes))}\",").appendLine()
-            stringBuilder.append("        \"description\":\"${item.description ?: noValue}\",").appendLine()
-            stringBuilder.append("        \"totalCost\":\"${getQuantityString(R.plurals.price_with_suffix, item.totalPrice.toInt(), formatPrice(item.totalPrice))}\",").appendLine()
-            stringBuilder.append("        \"urgency\":\"${if (item.isUrgent) "!" else noValue}\"").appendLine()
-            stringBuilder.append("      }${if (i < purchaseDetail.item.size - 1) "," else ""}").appendLine()
+        return buildString {
+            appendLine("  {")
+            appendLine("""    "date":${purchaseDetail.purchase.date.time / 1000},""")
+            appendLine("""    "store": {""")
+            appendLine("""      "name":"${purchaseDetail.store?.name ?: noValue}",""")
+            appendLine("""      "category":"$storeCategory",""")
+            appendLine("""      "location": {""")
+            appendLine("""        "latitude":${purchaseDetail.store?.location?.latitude ?: noValue},""")
+            appendLine("""        "longitude":${purchaseDetail.store?.location?.longitude ?: noValue}""")
+            appendLine("      }")
+            appendLine("    },")
+            appendLine("""    "items": [""")
+            for ((i, item) in purchaseDetail.item.withIndex()) {
+                appendLine("      {")
+                appendLine("""        "name":"${item.name}",""")
+                appendLine("""        "quantity":"${getString(R.string.item_quantity, item.quantity.value, getString(item.quantity.unit.nameRes))}",""")
+                appendLine("""        "description":"${item.description ?: noValue}",""")
+                appendLine("""        "totalCost":"${getQuantityString(R.plurals.price_with_suffix, item.totalPrice.toInt(), formatPrice(item.totalPrice))}",""")
+                appendLine("""        "urgency":"${if (item.isUrgent) "!" else noValue}"""")
+                appendLine("      }${if (i < purchaseDetail.item.size - 1) "," else ""}")
+            }
+            appendLine("    ]")
+            appendLine("  }${if (isLast) "" else ","}")
         }
-        stringBuilder.append("    ]").appendLine()
-        stringBuilder.append("  }")
-        return stringBuilder.toString()
     }
 
     private fun getString(@StringRes id: Int) = application.getString(id)
