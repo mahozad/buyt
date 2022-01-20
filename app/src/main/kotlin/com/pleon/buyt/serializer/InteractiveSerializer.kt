d874d6fc7fbf6357b48700544d3e8517dfd2c71d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-01-20 20:51:16 +0330

    Merge all the serializer interfaces in a single file

--- app/src/main/kotlin/com/pleon/buyt/serializer/InteractiveSerializer.kt
+++
@@ -1,6 +0,0 @@
-package com.pleon.buyt.serializer
-
-/**
- * A serializer that does not require creating the output file itself.
- */
-interface InteractiveSerializer<T> : Serializer<T>
--- app/src/main/kotlin/com/pleon/buyt/serializer/Serializer.kt
+++ app/src/main/kotlin/com/pleon/buyt/serializer/Serializer.kt
@@ -1,9 +1,21 @@
 package com.pleon.buyt.serializer
 
-interface Serializer<T> {
+/* sealed */ interface Serializer<T> {
     val mimeType: String
     val fileExtension: String
     var updateListener: (suspend (progress: Int, fragment: String) -> Unit)?
     var finishListener: (suspend () -> Unit)?
     suspend fun serialize(entities: List<T>)
 }
+
+/**
+ * A serializer that requires creating the output file directly itself.
+ */
+interface StandaloneSerializer<T> : Serializer<T> {
+    val defaultOutputFileName: String
+}
+
+/**
+ * A serializer that does not require creating the output file itself.
+ */
+interface InteractiveSerializer<T> : Serializer<T>
