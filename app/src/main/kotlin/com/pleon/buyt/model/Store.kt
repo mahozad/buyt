ee2f9a8ef9bff28ae705c1c4980b54d590dc6a55
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-01-23 12:44:55 +0330

    Reformat code

--- app/src/main/kotlin/com/pleon/buyt/model/Store.kt
+++ app/src/main/kotlin/com/pleon/buyt/model/Store.kt
@@ -4,10 +4,16 @@ import androidx.room.*
 import java.io.Serializable
 
 @Entity(indices = [Index("latitude", "longitude")])
-class Store(@Embedded val location: Coordinates, val name: String, val category: Category) : Serializable {
+class Store(
+    @Embedded
+    val location: Coordinates,
+    val name: String,
+    val category: Category
+) : Serializable {
 
     @PrimaryKey(autoGenerate = true)
     var storeId: Long = 0
+
     // To fix the bug that happens when two stores are deleted in a row (the first appears again)
     var isFlaggedForDeletion = false
 
