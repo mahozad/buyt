ccf0ce0b222da725355ecde84f235ab712c1f53d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-02-11 17:11:42 +0330

    Reformat code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 // This file is used to tell Gradle which modules it should include when building the application
 // If the project expands to use multiple modules, they need to be added here.
 rootProject.name = "Buyt"
-include (":app")
+include(":app")
